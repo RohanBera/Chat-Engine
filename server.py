2a093827c1f652b4592d8ea38f0c1706610f116a
Subhash Sarangi <32383953+Subhash3@users.noreply.github.com>
2019-10-01 15:21:55 +0530

    Update server.py
    Added exception handlers to the server.py file.

--- server.py
+++ server.py
@@ -2,11 +2,21 @@ import socket
 
 host = '10.5.1.84'
 port = 9000
+Max_Connections = 5
 
-srv_sock = socket.socket() # creates a socket
-srv_sock.bind((host, port))
+try :
+    srv_sock = socket.socket() # creates a socket
+    print("Socket is created.!")
+except Exception as e :
+    print("Error while creating a socket: ", e)
+   
+try :
+    srv_sock.bind((host, port))
+    print("Socket is bound to Host IP: ", host, " and Port: ", port)
+except Exception as e :
+    print("Error while binding the socket: ", e)
 # while True:
-srv_sock.listen()
+srv_sock.listen(Max_connections)
 
 conn, addr = srv_sock.accept()
 print("connection established : "+ str(addr))
